0e276dcdb05d1baaa8ee3c36edcacb45816a6030
akshatshrivastava <65461217+akshatshrivastava@users.noreply.github.com>
2020-05-20 12:32:53 +0530

    Add files via upload

---
+++ logistic reg.R
@@ -0,0 +1,161 @@
+library(ggplot2)
+library(cowplot)
+
+url <- "http://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+
+data <- read.csv(url, header=FALSE)
+
+#####################################
+##
+## Reformat the data so that it is
+## 1) Easy to use (add nice column names)
+## 2) Interpreted correctly by glm()..
+##
+#####################################
+head(data) # you see data, but no column names
+
+colnames(data) <- c(
+  "age",
+  "sex",# 0 = female, 1 = male
+  "cp", # chest pain
+  # 1 = typical angina,
+  # 2 = atypical angina,
+  # 3 = non-anginal pain,
+  # 4 = asymptomatic
+  "trestbps", # resting blood pressure (in mm Hg)
+  "chol", # serum cholestoral in mg/dl
+  "fbs",  # fasting blood sugar if less than 120 mg/dl, 1 = TRUE, 0 = FALSE
+  "restecg", # resting electrocardiographic results
+  # 1 = normal
+  # 2 = having ST-T wave abnormality
+  # 3 = showing probable or definite left ventricular hypertrophy
+  "thalach", # maximum heart rate achieved
+  "exang",   # exercise induced angina, 1 = yes, 0 = no
+  "oldpeak", # ST depression induced by exercise relative to rest
+  "slope", # the slope of the peak exercise ST segment
+  # 1 = upsloping
+  # 2 = flat
+  # 3 = downsloping
+  "ca", # number of major vessels (0-3) colored by fluoroscopy
+  "thal", # this is short of thalium heart scan
+  # 3 = normal (no cold spots)
+  # 6 = fixed defect (cold spots during rest and exercise)
+  # 7 = reversible defect (when cold spots only appear during exercise)
+  "hd" # (the predicted attribute) - diagnosis of heart disease
+  # 0 if less than or equal to 50% diameter narrowing
+  # 1 if greater than 50% diameter narrowing
+)
+
+head(data) # now we have data and column names
+
+str(data) # this shows that we need to tell R which columns contain factors
+# it also shows us that there are some missing values. There are "?"s
+# in the dataset. These are in the "ca" and "thal" columns...
+
+## First, convert "?"s to NAs...
+data[data == "?"] <- NA
+
+## Now add factors for variables that are factors and clean up the factors
+## that had missing data...
+data[data$sex == 0,]$sex <- "F"
+data[data$sex == 1,]$sex <- "M"
+data$sex <- as.factor(data$sex)
+
+data$cp <- as.factor(data$cp)
+data$fbs <- as.factor(data$fbs)
+data$restecg <- as.factor(data$restecg)
+data$exang <- as.factor(data$exang)
+data$slope <- as.factor(data$slope)
+
+data$ca <- as.integer(data$ca) # since this column had "?"s in it
+# R thinks that the levels for the factor are strings, but
+# we know they are integers, so first convert the strings to integers...
+data$ca <- as.factor(data$ca)  # ...then convert the integers to factor levels
+
+data$thal <- as.integer(data$thal) # "thal" also had "?"s in it.
+data$thal <- as.factor(data$thal)
+
+## This next line replaces 0 and 1 with "Healthy" and "Unhealthy"
+data$hd <- ifelse(test=data$hd == 0, yes="Healthy", no="Unhealthy")
+data$hd <- as.factor(data$hd) # Now convert to a factor
+
+str(data) ## this shows that the correct columns are factors
+
+nrow(data[is.na(data$ca)|is.na(data$thal),])
+data[is.na(data$ca)|is.na(data$thal),]
+
+#total rows before deletion on NA rows
+nrow(data)
+
+#deletion of NA rows from data
+data<-data[!(is.na(data$ca) | is.na(data$thal)),]
+
+#total rows after deletion
+nrow(data)
+
+#now we need to make sure that dieseased people come from both male 
+#and female. If no female, we can remove that column.
+
+#xtabs is used to extract data from two sp. columns
+xtabs(~ hd + sex, data = data)
+#it shows that 25 females have hd, therefore cant be removed.
+
+#do same thing for all categorical variables and other columns
+xtabs(~ hd + cp, data=data)
+
+xtabs(~hd+fbs, data=data)
+
+xtabs(~hd+restecg, data=data)
+
+xtabs(~ hd + exang, data=data)
+
+xtabs(~ hd + slope, data=data)
+
+xtabs(~ hd + ca, data=data)
+
+xtabs(~ hd + thal, data=data)
+
+#Logistic Regression
+logistic<- glm(hd~sex, data=data, family="binomial")
+summary(logistic)
+
+## Now calculate the overall "Pseudo R-squared" and its p-value
+ll.null <- logistic$null.deviance/-2
+ll.proposed <- logistic$deviance/-2
+
+## McFadden's Pseudo R^2 = [ LL(Null) - LL(Proposed) ] / LL(Null)
+(ll.null - ll.proposed) / ll.null
+
+## The p-value for the R^2
+1 - pchisq(2*(ll.proposed - ll.null), df=(length(logistic$coefficients)-1))
+
+## now we can plot the data
+predicted.data <- data.frame(
+  probability.of.hd=logistic$fitted.values,
+  hd=data$hd)
+
+predicted.data <- predicted.data[
+  order(predicted.data$probability.of.hd, decreasing=FALSE),]
+predicted.data$rank <- 1:nrow(predicted.data)
+
+## Lastly, we can plot the predicted probabilities for each sample having
+## heart disease and color by whether or not they actually had heart disease
+ggplot(data=predicted.data, aes(x=rank, y=probability.of.hd)) +
+  geom_point(aes(color=hd), alpha=1, shape=4, stroke=2) +
+  xlab("Index") +
+  ylab("Predicted probability of getting heart disease")
+
+ggsave("heart_disease_probabilities.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
