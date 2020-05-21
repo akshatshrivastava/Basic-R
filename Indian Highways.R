ae8efe71cb53320482edc1d712b385e91d200bac
Akshat Shrivastava <65461217+akshatshrivastava@users.noreply.github.com>
2020-05-21 13:20:09 +0530

    Update Indian Highways.R

--- Indian Highways.R
+++ Indian Highways.R
@@ -1,4 +1,4 @@
-#reading data from csv file which is present in same Github directory
+#reading data from csv file which is present in same Github directory files used - dataforhighways.csv
 data<-read.csv("G:/Akshat data/totallengthofnh.csv", header=TRUE)
 head(data)
 
